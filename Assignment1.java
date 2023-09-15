8f8e62128e895eade2f889f7bbe883ca25a2988a
ngarciadanila <143655749+ngarciadanila@users.noreply.github.com>
2023-09-15 18:51:39 -0400

    tab alignment test

--- Assignment1.java
+++ Assignment1.java
@@ -5,12 +5,12 @@ public class Assignment1 {
     // File: Assignment1.java
     
     
-    int degF;
-    int degC;
-    String longChar;
-    String shortChar;
+    int degF; //input fahrenheit
+    int degC; //output celcius
+    String longChar; //input 5 character string
+    String shortChar; // output reverse 3 character string
 
-    System.out.println ("NNNN          NNN");
+    System.out.println ("NNNN          NNN \t\t\t hello");
     System.out.println ("NNNNNN        NNN");
     System.out.println ("NNN NNN       NNN");
     System.out.println ("NNN  NNN      NNN");
