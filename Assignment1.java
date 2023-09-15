285e168de624e0187c8057278bc62b7ad3aaed66
ngarciadanila <143655749+ngarciadanila@users.noreply.github.com>
2023-09-15 18:48:21 -0400

    make capital n

--- Assignment1.java
+++ Assignment1.java
@@ -4,14 +4,20 @@ public class Assignment1 {
     // Assignment 1: printing, equations, manipulating strings, integers, concating strings
     // File: Assignment1.java
     
-    Scanner scan = new Scanner(System.in);
+    
     int degF;
     int degC;
     String longChar;
     String shortChar;
 
     System.out.println ("NNNN          NNN");
-    System.out.println ("NNNNNN    NNN");
-    System.out.println ("NNN NNN   NNN");
+    System.out.println ("NNNNNN        NNN");
+    System.out.println ("NNN NNN       NNN");
+    System.out.println ("NNN  NNN      NNN");
+    System.out.println ("NNN   NNN     NNN");
+    System.out.println ("NNN    NNN    NNN");
+    System.out.println ("NNN     NNNN  NNN");
+    System.out.println ("NNN       NNNNNNN");
+
   }
 }
